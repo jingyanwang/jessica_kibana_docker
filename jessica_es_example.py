eeab9dbcd446d8938f8cb290ad1448f981e18f02
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2021-07-07 10:45:31 +0400

    Add files via upload

--- jessica_es_example.py
+++ jessica_es_example.py
@@ -1,7 +1,13 @@
 ########jessica_es_example.py########
 import jessica_es
 
-es_session = jessica_es.start_es('/jessica/elasticsearch-6.7.1')
+es_session = jessica_es.start_es(
+	es_path = "/jessica/elasticsearch-6.7.1",
+	es_port_number = "9466")
+
+'''
+http://0.0.0.0:9466
+'''
 
 jessica_es.insert_doc_to_es(
 	es_session,
@@ -10,7 +16,7 @@ jessica_es.insert_doc_to_es(
 	doc_id = 'c')
 
 '''
-http://0.0.0.0:2897/a/_search?pretty=true
+http://0.0.0.0:9466/a/_search?pretty=true
 '''
 
 for r in jessica_es.search_doc_by_match(
@@ -21,5 +27,14 @@ for r in jessica_es.search_doc_by_match(
 	return_entity_max_number = 1,
 	return_entity_min_score = 0):
 	print(r)
+
+jessica_es.start_kibana(
+	kibana_port_number = "5145",
+	es_port_number = "9466",
+	)
+
+'''
+http://0.0.0.0:5145
+'''
 
 ########jessica_es_example.py########
