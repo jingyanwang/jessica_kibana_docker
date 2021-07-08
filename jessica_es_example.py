95644be35e23883372898fb66b47fd108f367429
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2021-07-08 20:59:20 +0400

    Add files via upload

--- jessica_es_example.py
+++ jessica_es_example.py
@@ -11,22 +11,27 @@ http://0.0.0.0:9466
 
 jessica_es.insert_doc_to_es(
 	es_session,
-	es_index = 'a',
-	doc_dict = {'a':'b'},
-	doc_id = 'c')
+	es_index = 'customers',
+	doc_dict = {'CustomerName':'Alfreds Futterkiste',
+		'Address':'Obere Str. 57',
+		'Age':26},
+	doc_id = '1')
 
 '''
-http://0.0.0.0:9466/a/_search?pretty=true
+http://0.0.0.0:9466/customers/_search?pretty=true
+http://192.168.1.103:9466/customers/_search?pretty=true
 '''
 
 for r in jessica_es.search_doc_by_match(
-	index_name = 'a',
-	entity_name = 'b',
-	field_name = 'a',
+	index_name = 'customers',
+	entity_name = 'Alfreds Futterkiste',
+	field_name = 'CustomerName',
 	es_session = es_session,
 	return_entity_max_number = 1,
 	return_entity_min_score = 0):
 	print(r)
+
+#########
 
 jessica_es.start_kibana(
 	kibana_port_number = "5145",
@@ -35,6 +40,7 @@ jessica_es.start_kibana(
 
 '''
 http://0.0.0.0:5145
+http://192.168.1.103:5145
 '''
 
 ########jessica_es_example.py########
