dac707960af6d160d3c8b5fa9539587993190194
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2021-08-31 11:15:34 +0400

    Add files via upload

--- jessica_es_example.py
+++ jessica_es_example.py
@@ -5,8 +5,12 @@ es_session = jessica_es.start_es(
 	es_path = "/jessica/elasticsearch-6.7.1",
 	es_port_number = "9466")
 
+es_session = jessica_es.start_es(
+	es_path = "/jessica/elasticsearch-7.14.0",
+	es_port_number = "9466")
+
 '''
-http://0.0.0.0:9466
+http://localhost:9466
 '''
 
 jessica_es.insert_doc_to_es(
@@ -17,8 +21,16 @@ jessica_es.insert_doc_to_es(
 		'Age':26},
 	doc_id = '1')
 
+
+jessica_es.ingest_json_to_es_index(
+	json_file = 'data_sample.json',
+	es_index = "customers",
+	es_session = es_session,
+	document_id_feild = 'CustomerName',
+	)
+
 '''
-http://0.0.0.0:9466/customers/_search?pretty=true
+http://localhost:9466/customers/_search?pretty=true
 '''
 
 for r in jessica_es.search_doc_by_match(
@@ -30,15 +42,23 @@ for r in jessica_es.search_doc_by_match(
 	return_entity_min_score = 0):
 	print(r)
 
+
 #########
 
 jessica_es.start_kibana(
+	kibana_path = '/jessica/kibana-6.7.1-linux-x86_64',
+	kibana_port_number = "5145",
+	es_port_number = "9466",
+	)
+
+jessica_es.start_kibana(
+	kibana_path = '/jessica/kibana-7.14.0-linux-x86_64',
 	kibana_port_number = "5145",
 	es_port_number = "9466",
 	)
 
 '''
-http://0.0.0.0:5145
+http://localhost:5145
 '''
 
 ########jessica_es_example.py########
