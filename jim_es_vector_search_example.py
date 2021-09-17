4b77967317bafc0cfe104e210d6c4667e1125955
Jing-Yan Wang <jywang.ieee@gmail.com>
2021-09-17 16:46:50 +0400

    Create jim_es_vector_search_example.py

---
+++ jim_es_vector_search_example.py
@@ -0,0 +1,33 @@
+#########jim_es_vector_search_example.py#########
+
+import jessica_es
+import jim_es_vector_search
+
+es_session = jessica_es.start_es(
+	es_path = "/jessica/elasticsearch-7.13.4",
+	es_port_number = "9466")
+
+jim_es_vector_search.build_vector_index(
+	index_name = "logo",
+	vector_dim_size = 3,
+	es_session = es_session,
+	vector_field_name = 'logo_embedding',
+	)
+
+jim_es_vector_search.ingest_json_to_es_index(
+	json_file = 'logo_data.json',
+	es_index = 'logo',
+	es_session = es_session,
+	document_id_feild = 'document_id',
+	)
+
+jim_es_vector_searchsearch_by_vector(
+	index_name = 'logo',
+	vector_field_name = 'logo_embedding',
+	query_vector = [1.0,2.2,3.3],
+	es_session = es_session,
+	similarity_measure = 'euclidean',
+	return_entity_max_number = 2,
+	)
+
+#########jim_es_vector_search_example.py#########
